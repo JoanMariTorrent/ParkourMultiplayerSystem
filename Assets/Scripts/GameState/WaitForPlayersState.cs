cef9ec6e5457cdfdb112d47c7a3af5af88cc35a3
JoanMari1C <joanmaritorrent@cev.com>
2025-09-21 13:06:06 +0200

    Update WaitForPlayersState.cs

--- Assets/Scripts/GameState/WaitForPlayersState.cs
+++ Assets/Scripts/GameState/WaitForPlayersState.cs
@@ -6,21 +6,26 @@ public class WaitForPlayersState : StateNode
 {
     [SerializeField] private int _minPlayers = 2;
 
+    private void Awake()
+    {
+        Debug.Log("Starting machine");
+    }
     public override void Enter(bool asServer)
     {
         base.Enter(asServer);
 
         if (!asServer)
             return;
-
+        Debug.Log("2");
         StartCoroutine(WaitForPlayers());
     }
 
     private IEnumerator WaitForPlayers()
     {
+        Debug.Log("3");
         while (networkManager.players.Count < _minPlayers)
             yield return null;
-
+        Debug.Log("4");
         machine.Next();
 
     }
